d26e4473750fddf2a139a6324825ad8ed128e896
bastengao <bastengao@gmail.com>
2019-08-22 00:36:53 +0800

    Add debug package

---
+++ debug/logger.go
@@ -0,0 +1,21 @@
+package debug
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var logger *log.Logger
+
+func init() {
+	file, err := os.OpenFile("log/debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+	logger = log.New(file, "logger: ", log.Lshortfile)
+}
+
+func Info(s ...interface{}) {
+	logger.Println(s)
+}
