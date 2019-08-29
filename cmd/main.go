01ae6933606fe16858d4abd8dc0340ed91442b18
bastengao <bastengao@gmail.com>
2019-08-29 14:16:51 +0800

    Expand absolute path

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"gncdu"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	dir := "."
 	if len(os.Args) > 1 {
 		dir = os.Args[1]
+	}
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	gncdu.ShowUI(func() []*gncdu.FileData {
